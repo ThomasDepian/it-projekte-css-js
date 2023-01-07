24d38622af55f9cc3f2c4fef29d7331409b6d2fd
Thomas Depian <thomas.depian99@gmail.com>
2023-01-07 12:46:04 +0100

    add some examples/showcases to the tasks

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-const NUM_STEPS = 18;
-const TASK_PATH = '/it-projekte-css-js/tasks';
+const NUM_TASKS = 18;
+// const TASK_PATH = '/it-projekte-css-js/tasks';
+const TASK_PATH = '/tasks';
 const PAGE_TITLE = 'Meine Pizza-Seite | CSS + JS Kurs - Task'
 
-let currentStep = 1;
+let currentTask = 1;
 
-function initialize() {
-    currentStep = 1;
-
-    // set max in progress bar
-    document.getElementById('progress').setAttribute('max', NUM_STEPS - 1);
-
-    // set max steps in footer
-    document.getElementById('steps').innerText = NUM_STEPS;
-    
-    updateToCurrentStep();
+function readTaskFromUrl() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+        });
+        
+    // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+    const taskNr = params.task || 1;
+    return taskNr;    
 }
 
-function updateToCurrentStep() {
+function initialize() {
+    // addEventListener('beforeunload', _ => {
+    //     return "Achtung! Deine bisherige Arbeit könnte verloren gehen. Bitte benutze zum navigieren ausschließlich die Knöpfe auf der Webseite. Willst du fortfahren?";
+    // });
+    // requestFullscreen();
+
+    currentTask = readTaskFromUrl();
+
+    // set max in progress bar
+    document.getElementById('progress').setAttribute('max', NUM_TASKS - 1);
+
+    // set max Task in footer
+    document.getElementById('num-tasks').innerText = NUM_TASKS;
+    
+    updateToCurrentTask();
+}
+
+function updateToCurrentTask() {
+
+    // update URL
+    window.history.pushState(undefined, `Task ${currentTask}`, `index.html?task=${currentTask}`);
+
     // update page title
-    document.title = `${PAGE_TITLE} ${currentStep}`;
+    document.title = `${PAGE_TITLE} ${currentTask}`;
 
     // load new task
-    document.getElementById('task').setAttribute('src', `${TASK_PATH}/task-${currentStep}.html`);
+    document.getElementById('task').setAttribute('src', `${TASK_PATH}/task-${currentTask}.html`);
 
     // update progress bar
-    document.getElementById('progress').value = currentStep - 1;
-    document.getElementById('progress').innerHTML = `${currentStep}/${NUM_STEPS}`;
+    document.getElementById('progress').value = currentTask - 1;
+    document.getElementById('progress').innerHTML = `${currentTask}/${NUM_TASKS}`;
 
-    // set current step in footer
-    document.getElementById('step').innerText = currentStep;
+    // set current task in footer
+    document.getElementById('num-task').innerText = currentTask;
 
-    if (currentStep == 1) {
+    if (currentTask == 1) {
         document.getElementById('prev').disabled = true;
     } else {
         document.getElementById('prev').disabled = false;
     }
 
-    if (currentStep == NUM_STEPS) {
+    if (currentTask == NUM_TASKS) {
         document.getElementById('next').disabled = true;
     } else {
         document.getElementById('next').disabled = false;
@@ -44,15 +64,15 @@ function updateToCurrentStep() {
 }
 
 function next() {
-    if (currentStep < NUM_STEPS) {
-        currentStep++;
-        updateToCurrentStep();
+    if (currentTask < NUM_TASKS) {
+        currentTask++;
+        updateToCurrentTask();
     }
 }
 
 function prev() {
-    if (currentStep > 1) {
-        currentStep--;
-        updateToCurrentStep();
+    if (currentTask > 1) {
+        currentTask--;
+        updateToCurrentTask();
     }
 }
