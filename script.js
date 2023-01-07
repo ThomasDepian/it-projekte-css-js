8d48728d4f587a7341036d4a8369b6641a859797
Thomas Depian <thomas.depian99@gmail.com>
2023-01-07 21:03:05 +0100

    improve functionality and style task 18

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 const NUM_TASKS = 18;
-// const TASK_PATH = '/it-projekte-css-js/tasks';
-const TASK_PATH = '/tasks';
+const TASK_PATH = '/it-projekte-css-js/tasks';
+// const TASK_PATH = '/tasks';
 const PAGE_TITLE = 'Meine Pizza-Seite | CSS + JS Kurs - Task'
 
 let currentTask = 1;
 
-function readTaskFromUrl() {
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-        });
-        
-    // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-    const taskNr = params.task || 1;
-    return taskNr;    
-}
-
 function initialize() {
-    // addEventListener('beforeunload', _ => {
-    //     return "Achtung! Deine bisherige Arbeit könnte verloren gehen. Bitte benutze zum navigieren ausschließlich die Knöpfe auf der Webseite. Willst du fortfahren?";
-    // });
-    // requestFullscreen();
-
-    currentTask = readTaskFromUrl();
+    currentTask = 1;
 
     // set max in progress bar
     document.getElementById('progress').setAttribute('max', NUM_TASKS - 1);
@@ -33,9 +18,6 @@ function initialize() {
 }
 
 function updateToCurrentTask() {
-
-    // update URL
-    window.history.pushState(undefined, `Task ${currentTask}`, `index.html?task=${currentTask}`);
 
     // update page title
     document.title = `${PAGE_TITLE} ${currentTask}`;
